b53af575543ad2f96714e298b0f00066fd6aa564
Nyi Say Min <hyhy3402@gmail.com>
2025-04-13 01:29:47 +0630

    Add drop down items and their actions

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -4,33 +4,46 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { formatCurrency } from "@/lib/formatters"
-import { useState } from "react"
+import { useActionState, useState } from "react"
 import { addProduct } from "../../_action/products"
+import { useFormStatus } from "react-dom"
+import { Textarea } from "@/components/ui/textarea"
 
 export function ProductForm() {
-    const [priceInCents, setPriceInCents] = useState<number>()
-    return <form action={addProduct} className="space-y-8">
+    const [error, action] = useActionState(addProduct,{})
+    const [priceInCents, setPriceInCents] = useState<string>("")
+    return <form action={action} className="space-y-8">
         <div className="space-y-2">
             <Label htmlFor="name">Name</Label>
             <Input type="text" id="name" name="name" required/>
+            {error.name && <div className="test-destructive">{error.name}</div>}
         </div>
         <div className="space-y-2">
             <Label htmlFor="priceInCents">Price In Cents</Label>
-            <Input type="number" id="priceInCents" name="priceInCents" required value={priceInCents} onChange={e => setPriceInCents(Number(e.target.value) || undefined)}/>
-            <div className="text-muted-foreground">{formatCurrency((priceInCents || 0)/100)}</div>
+            <Input type="number" id="priceInCents" name="priceInCents" required value={priceInCents} onChange={e => setPriceInCents(e.target.value)}/>
+            <div className="text-muted-foreground">{formatCurrency((Number(priceInCents) || 0)/100)}</div>
+            {error.priceInCents && <div className="test-destructive">{error.priceInCents}</div>}
         </div>
         <div className="space-y-2">
             <Label htmlFor="description">Description</Label>
-            <Input id="description" name="description" required/>
+            <Textarea id="description" name="description" required/>
+            {error.description && <div className="test-destructive">{error.description}</div>}
         </div>
         <div className="space-y-2">
             <Label htmlFor="file">File</Label>
             <Input type="file" id="file" name="file" required/>
+            {error.file && <div className="test-destructive">{error.file}</div>}
         </div>
         <div className="space-y-2">
             <Label htmlFor="image">Image</Label>
             <Input type="file" id="image" name="image" required/>
+            {error.image && <div className="test-destructive">{error.image}</div>}
         </div>
-        <Button type="submit">Save</Button>
+        <SubmitButton/>
     </form>
+}
+
+function SubmitButton() {
+    const {pending} = useFormStatus()
+    return <Button type="submit" disabled={pending}>{pending ? "Saving..." : "Save"}</Button>
 }
--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,7 +1,12 @@
 import { Button } from "@/components/ui/button";
+import db from "@/db/db"
 import { PageHeader } from "../_components/PageHeader";
 import Link from "next/link";
-import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { CheckCircle2, MoreVertical, XCircle } from "lucide-react";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { ActiveToggleDropdownItem, DeleteDropdownItem } from "./_components/ProductAction";
 
 export default function AdminProductPage() {
     return <>
@@ -15,7 +20,20 @@ export default function AdminProductPage() {
     </>
 }
 
-function ProductsTable() {
+async function ProductsTable() {
+    const products = await db.product.findMany({ 
+        select: { 
+            id: true,
+            name: true, 
+            priceInCents: true, 
+            isAvailableForPurchase: true, 
+            _count: {select : {orders: true}}
+        },
+        orderBy: {name : "asc"}
+    })
+
+    if(products.length === 0) return <p>No products found</p>
+
     return <Table>
         <TableHeader>
             <TableRow>
@@ -30,6 +48,49 @@ function ProductsTable() {
                 </TableHead>
             </TableRow>
         </TableHeader>
-        
+        <TableBody>
+            {products.map(product => (
+                <TableRow key={product.id}>
+                    <TableCell>
+                        {product.isAvailableForPurchase ? (
+                            <>
+                                <span className="sr-only">Available</span>
+                                <CheckCircle2/>
+                            </>
+                        ) : (
+                            <>
+                                <span className="sr-only">Unavailable</span>
+                                <XCircle className="stroke-destructive"/>
+                            </>
+                        )
+                        }
+                    </TableCell>
+                    <TableCell>{product.name}</TableCell>
+                    <TableCell>{formatCurrency(product.priceInCents / 100)}</TableCell>
+                    <TableCell>{formatNumber(product._count.orders)}</TableCell>
+                    <TableCell>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger>
+                                <MoreVertical/>
+                                <span className="sr-only">Actions</span>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent>
+                                <DropdownMenuItem asChild>
+                                    <a download href={`/admin/products/${product.id}/download`}>
+                                        Download
+                                    </a>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem asChild>
+                                    <Link href={`/admin/product/${product.id}/edit`}>Edit</Link>
+                                </DropdownMenuItem>
+                                <ActiveToggleDropdownItem id={product.id} isAvailableForPurchase={product.isAvailableForPurchase}/>
+                                <DropdownMenuSeparator/>
+                                <DeleteDropdownItem id={product.id} disabled={product._count.orders > 0}/>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </TableCell>
+                </TableRow>
+            ))}
+        </TableBody>
     </Table>
 }
